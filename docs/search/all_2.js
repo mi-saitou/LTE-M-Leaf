0a8995f342efff3805a9893bd00b863517477b49
K.Nakamura <kazu.nyan2020@gmail.com>
2021-01-22 10:45:27 +0900

    add docs

---
+++ docs/search/all_2.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['clrheader_5',['clrHeader',['../class_lpwa_http_client.html#a16f4302edf4074499f2257af4431d06d',1,'LpwaHttpClient']]],
+  ['connect_6',['connect',['../class_lpwa_client.html#ace36ed33257255b6e3cefa00a20de07f',1,'LpwaClient::connect(String host, uint16_t port)'],['../class_lpwa_client.html#ab0c218ed5e95249d1f50e7bd3814d330',1,'LpwaClient::connect(IPAddress ip, uint16_t port)'],['../class_lpwa_client.html#aa604a3476632e7152285da45ccddb37f',1,'LpwaClient::connect(const char *host, uint16_t port)']]],
+  ['connected_7',['connected',['../class_lpwa_client.html#aa5f555b19eaef684357a8a43e8a3a2ca',1,'LpwaClient']]]
+];
